b69a2fc753b5bfb4e31fa14dd366a831d24376f5
Shinpei Maruyama <shinpeim@gmail.com>
2012-06-01 05:06:31 +0900

    品詞だけでmatchさせていたのを、surfaceも見れように変えた

--- lib/totsuzenizer/filter.rb
+++ lib/totsuzenizer/filter.rb
@@ -10,11 +10,17 @@ module Totsuzenizer
     def totsuzenize(text)
       words = parse(text)
       words = words.select { |e| e.surface != "BOS/EOS"}
+      words = words.map{|w|
+        {
+          :surface    => w.surface,
+          :word_class => w.left.text.split(",")[0]
+        }
+      }
 
       index = 0
       while ! words.empty?
         if match?(words)
-          ret =  words.map{|w|w.surface}.join + T::TOTSUZENSHI_AA
+          ret =  words.map{|w|w[:surface]}.join + T::TOTSUZENSHI_AA
           return ret
         end
         words.pop
@@ -26,15 +32,28 @@ module Totsuzenizer
     private
 
     def patterns
-      [["名詞","助詞"]]
+      [[{:word_class => "名詞"}, {:word_class => "助詞", :surface => "の"}].freeze,
+       [{:word_class => "動詞"}, {:word_class => "助詞", :surface => "て"}].freeze,
+       [{:word_class => "名詞"}, {:word_class => "助動詞", :surface => "だ"}, {:word_class => "助詞", :surface => "と"}].freeze]
     end
 
     #wordsの末尾がマッチするかチェック
     def match?(words)
-      word_classes = words.map{|w| w.left.text.split(',')[0]}
-      patterns.each do |pat|
-        next if pat.size > word_classes.size
-        return true if pat == word_classes.last(pat.size)
+      patterns.each do |pattern|
+        next if pattern.size > words.size
+
+        matched = true
+        last_words = words.last(pattern.size)
+
+        0.upto(pattern.size-1) { |index|
+          #patternとwords両方に存在するキーについて同値チェック
+          keys_to_check = last_words[index].keys & pattern[index].keys
+          unless ( keys_to_check.all?{ |key| last_words[index][key] == pattern[index][key]} )
+            matched = false
+          end
+        }
+
+        return true if matched
       end
 
       return false
--- spec/totsuzenize_spec.rb
+++ spec/totsuzenize_spec.rb
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
 require File.join(File.expand_path(File.dirname(__FILE__)) , 'spec_helper')
 
-TOTSUZENSHI_AA = "
-＿人人人人人＿
-＞ 突然の死 ＜
-￣ＹＹＹＹＹ￣"
-
 describe T::Filter do
   before do
     @filter = T::Filter.new(File.join(T.root, 'okura-dic'))
   end
 
-  context '「名詞 + 助詞」が含まれるとき' do
-    it "一番後ろの「名詞 + 助詞」でマッチして「突然の死」を返す" do
-      @filter.totsuzenize("あの夏で待ってるっていうアニメを好む人間は良いやつだと思う").should ==
-        "あの夏で待ってるっていうアニメを好む人間は" + TOTSUZENSHI_AA
+  context '「動詞 + て(助詞)」が含まれるとき' do
+    it "一番後ろの「動詞 + て」でマッチして「突然の死」を返す" do
+      @filter.totsuzenize("それから少し間を置いて、私は部屋に帰った").should ==
+        "それから少し間を置いて" + T::TOTSUZENSHI_AA
+    end
+  end
+
+  context '「名詞 + の(助詞)」が含まれるとき' do
+    it "一番後ろの「名詞 + の」でマッチして「突然の死」を返す" do
+      @filter.totsuzenize("この前新宿のユニクロにいったとき、彼氏のサンダルを買ったんだけど").should ==
+        "この前新宿のユニクロにいったとき、彼氏の" + T::TOTSUZENSHI_AA
+    end
+  end
+
+  context '「名詞 + だ(助詞) + と(助詞)」が含まれるとき' do
+    it "一番後ろの「名詞 + だ + と」でマッチして「突然の死」を返す" do
+      @filter.totsuzenize("ご飯を食べるときに、あんまり周りが静かだと緊張する").should ==
+        "ご飯を食べるときに、あんまり周りが静かだと" + T::TOTSUZENSHI_AA
+    end
+  end
+
+  context '何も含まれないとき' do
+    it "nilを返す" do
+      @filter.totsuzenize("XXXXXXXXXXXXXXX").should == nil
     end
   end
 end
